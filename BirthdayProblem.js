a215b24bdcf90b56265c8155f325fb3cb56262f4
Hong <emily_harmony@msn.com>
2021-06-06 16:01:24 -0500

    Update BirthdayProblem.js

--- BirthdayProblem.js
+++ BirthdayProblem.js
@@ -1,3 +1,5 @@
+//Calculate P(A′), the probability that no two people in the room have the same birthday. 
+//  i = P(A′) = number of permutations of k different numbers/number of all permutations
 let k=1, i=1;
 for (k=1; i>0.5; k++) {
     i*=(366-k)/365;
@@ -7,3 +9,5 @@ console.log(i,'\n', k-1);
 // https://playcode.io/new/
 //0.4927027656760144
 //23
+
+//Replace 0.5 with 0.8
